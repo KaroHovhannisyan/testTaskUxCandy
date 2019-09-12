af4dbc3ceb4cbb8488bb5c3a4fcf4b48b781e880
KaroHovhannisyan <kar.hovhannisyan.95@gmail.com>
2019-09-12 17:59:10 +0400

    Fixes

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import {Login, HomePage} from "../containers";
+import {Login, HomePage, } from "../containers";
+import {MainLayout } from "../layouts";
 import {Switch, Route} from "react-router-dom";
 import {LOGIN_PATH, MAIN_PATH, TABLE_PATH} from "../common/constants";
-import Header from "../containers/Header/Header"
 import TablePage from "../containers/TablePage/TablePage";
 
 const Routes = () => (
-    <Switch>
-        <div>
-            <Header/>
-            <Route path={LOGIN_PATH} component={Login}/>
-            <Route path={MAIN_PATH} component={HomePage}/>
-            <Route path={TABLE_PATH} component={TablePage}/>
-        </div>
-    </Switch>
+        <MainLayout>
+            <Switch>
+                <Route path={LOGIN_PATH} component={Login}/>
+                <Route path={MAIN_PATH} component={HomePage}/>
+                <Route path={TABLE_PATH} component={TablePage}/>
+            </Switch>
+        </MainLayout>
+
 );
 
 export default Routes;
